8501c74bb579f5eeba99322f5e20cdb99d05c1d5
tomo.yoshi <yoto445katuo@gmail.com>
2021-03-22 08:22:37 +0000

    user registration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,29 +8,11 @@
     </head>
 
     <body>
-        <header class="mb-4">
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                {{-- トップページへのリンク --}}
-                <a class="navbar-brand" href="/">TaskList</a>
-
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="nav-bar">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <ul class="navbar-nav">
-                        {{-- メッセージ作成ページへのリンク --}}
-                　　　　<li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'nav-link']) !!}</li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-
+       
         <div class="container">
-            
             {{-- エラーメッセージ --}}
             @include('commons.error_messages')
+            @include('commons.navbar')
             
             @yield('content')
         </div>
