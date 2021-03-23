2245aababcb61a5a808fcef08f2d293490626353
tomo.yoshi <yoto445katuo@gmail.com>
2021-03-23 10:09:27 +0000

    modified-taskcontroller

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -90,18 +90,19 @@ class TasksController extends Controller
      */
     public function show($id)
     {
-        if (\Auth::check()) { // 認証済みの場合
-            // 認証済みユーザを取得
-            $user = \Auth::user();
-        
-            // idの値でメッセージを検索して取得
-            $task = Task::findOrFail($id);
+       
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
 
-            // メッセージ詳細ビューでそれを表示
-            return view('tasks.show', [
-                'task' => $task,
-            ]);
+        if (\Auth::id() === $task->user_id) {
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
         }
+        
+         // トップページへリダイレクトさせる
+        return redirect('/');
+        
     }
 
     /**
@@ -112,18 +113,20 @@ class TasksController extends Controller
      */
     public function edit($id)
     {
-        if (\Auth::check()) { // 認証済みの場合
-            // 認証済みユーザを取得
-            $user = \Auth::user();
-        
-            // idの値でメッセージを検索して取得
-            $task = Task::findOrFail($id);
-
-            // メッセージ編集ビューでそれを表示
-            return view('tasks.edit', [
-                'task' => $task,
+      
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+            
+            if (\Auth::id() === $task->user_id) {
+                   
+                // メッセージ編集ビューでそれを表示
+                return view('tasks.edit', [
+                    'task' => $task,
             ]);
-        }
+            }
+            
+         // トップページへリダイレクトさせる
+        return redirect('/');
     }
         
         
@@ -147,13 +150,15 @@ class TasksController extends Controller
         $task = Task::findOrFail($id);
         
         // メッセージを更新
-        $task->status = $request->status;    // 追加
-        $task->content = $request->content;
-        $task->save();
-
+        if (\Auth::id() === $task->user_id) { //自分だけ更新可能
+            $task->status = $request->status;    // 追加
+            $task->content = $request->content;
+            $task->save();
+        }
         // トップページへリダイレクトさせる
         return redirect('/');
-    }
+    
+    }    
 
     /**
      * Remove the specified resource from storage.
@@ -175,3 +180,4 @@ class TasksController extends Controller
         return redirect('/');
     }
 }
+    
